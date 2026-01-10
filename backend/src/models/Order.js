0fae3ca7596008227adad735cf91a5e36d0ff7cf
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2026-01-10 16:49:25 +0530

    add the order schema into sys

---
+++ backend/src/models/Order.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        items: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'Product',
+                    required: true,
+                },
+                size: {
+                    type: String,
+                    required: true,
+                },
+                quantity: {
+                    type: Number,
+                    required: true,
+                },
+                price: {
+                    type: Number,
+                    required: true, // price at checkout time
+                },
+            },
+        ],
+        totalPrice: {
+            type: Number,
+            required: true,
+        },
+        orderDate: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Order', orderSchema);
