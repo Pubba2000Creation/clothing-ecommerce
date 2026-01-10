c3545866df10ea5f43e28d2b258cf9842914d5fe
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2026-01-10 12:50:15 +0530

    inizied the server entry

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+
+connectDB();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
