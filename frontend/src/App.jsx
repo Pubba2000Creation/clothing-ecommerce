002f253aade2eb159a7b281c29b4c9ac1166c45e
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2026-01-20 13:07:39 +0530

    add production page amd it css and conncet the api for the rendering data from the backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './pages/Home';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
