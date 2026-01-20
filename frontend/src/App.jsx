1e2ecdeff6f272b357ffce05316b0221a6a335e1
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2026-01-20 13:09:05 +0530

    add aboutus page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import About from './pages/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
